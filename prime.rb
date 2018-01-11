0d6cb9d1c4a9349dd1eff5e6f7a4c4aca8913fd1
andrewkjankowski <andrewkjankowski@gmail.com>
2018-01-11 15:37:06 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  num_factors = 2
+  if num < 2
+    return false
+  else
+    for i in 2...num do
+      if num % i == 0
+        num_factors += 1
+      end
+    end
+
+    return num_factors == 2
+  end
+end
